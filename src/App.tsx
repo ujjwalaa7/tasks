db9e351a233bfc3279386b88b4dd0577e195c319
Ujjwala <ujjwalap@udel.edu>
2024-03-05 21:19:34 -0500

    Passed tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
+
 import { Button, Col, Container, Row } from "react-bootstrap";
-=======
+
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
-
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
+            HEAD
             <h1>Software Development Task 3</h1>
             <img
                 src="https://i.pinimg.com/564x/23/c2/e0/23c2e029107dae85d9601439cf3ffd04.jpg"
@@ -45,7 +43,6 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. - Hello World ujjwala pothula
             </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -58,7 +55,7 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
+            upstream/task-state
         </div>
     );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -7,10 +7,28 @@ import { Button } from "react-bootstrap";
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
  */
+
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [die1, setDice1] = useState<number>(2);
+    const [die2, setDice2] = useState<number>(1);
+    const leftRoll = () => {
+        setDice1(d6);
+    };
+    const rightRoll = () => {
+        setDice2(d6);
+    };
+    return (
+        <div>
+            <span data-testid="left-die">{die1}</span>
+            <span data-testid="right-die">{die2}</span>
+            <Button onClick={leftRoll}>Roll Left</Button>
+            <Button onClick={rightRoll}>Roll Right</Button>
+            <Button>{die1 === die2 && die1 !== 1 && <div>Win</div>}</Button>
+            <Button>{die1 === die2 && die1 === 1 && <div>Lose</div>}</Button>
+        </div>
+    );
 }
